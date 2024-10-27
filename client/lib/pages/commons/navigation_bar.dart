9a12f92b0a5c15f2c0ccd517d1b362d18af5a9c0
seg <segnities007@gmail.com>
2024-10-27 23:42:04 +0900

    add navigationbar

---
+++ client/lib/pages/commons/navigation_bar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MakerNavigationBar extends StatelessWidget {
+  const MakerNavigationBar({
+    super.key,
+    required this.index,
+    required this.changeIndex,
+    required this.icons,
+    required this.labels,
+  });
+
+  final int index;
+  final void Function(int) changeIndex;
+  final List<IconData> icons;
+  final List<String> labels;
+
+  @override
+  Widget build(context) {
+    return NavigationBar(
+      shadowColor: Colors.green,
+      onDestinationSelected: changeIndex,
+      selectedIndex: index,
+      destinations: [
+        for(int i=0; i<icons.length; i++)
+          NavigationDestination(
+            icon: Icon(icons[i]),
+            label: labels[i],
+        )
+      ]
+    );
+  }
+}
